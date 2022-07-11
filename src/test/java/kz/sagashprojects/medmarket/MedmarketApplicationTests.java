7e4dadb1d64a8bdfd32ac50c2e997e16bcceebe7
Argyn Sagash <argyn.sagash@gmail.com>
2022-07-11 22:46:14 +0600

    Initial commit

---
+++ src/test/java/kz/sagashprojects/medmarket/MedmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sagashprojects.medmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
